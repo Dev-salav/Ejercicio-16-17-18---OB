d90ed3ec5df7e0b2eeeb4e0e5975fc870bb3822e
Dev-salav <de.salav24@gmail.com>
2022-07-22 11:12:06 -0500

    Ejercicio VF

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import Layout from "./components/pure/forms/layout";
 import FormularioTask from "./components/pure/forms/formAddTask";
 import FormLogin from "./components/pure/forms/formLogin";
 import Notfoundpage from "./pages/404";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  // let loggedIn = false;
+  let LoggedIn = true;
+
+  const Protected = ({ LoggedIn, children }) => {
+    if (!LoggedIn) {
+      return <Navigate to="/" replace />;
+    }
+    return children;
+  };
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<FormLogin />} />
-          <Route path="formtask" element={<FormularioTask />} />
+          <Route
+            path="/formtask"
+            element={
+              <Protected LoggedIn={LoggedIn}>
+                <FormularioTask />
+              </Protected>
+            }
+          />
           <Route path="*" element={<Notfoundpage />} />
         </Route>
       </Routes>
